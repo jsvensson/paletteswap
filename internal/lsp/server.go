30960906d1095b8e8aa1673daaa824e86b358bcf
Johan Svensson <johan@spiralmind.io>
2026-02-07 20:01:56 +0100

    feat: wire diagnostics publishing to document sync

--- internal/lsp/server.go
+++ internal/lsp/server.go
@@ -1,6 +1,8 @@
 package lsp
 
 import (
+	"sync"
+
 	"github.com/tliron/glsp"
 	protocol "github.com/tliron/glsp/protocol_3_16"
 	"github.com/tliron/glsp/server"
@@ -15,12 +17,15 @@ type Server struct {
 	handler protocol.Handler
 	docs    *DocumentStore
 	version string
+	mu      sync.RWMutex
+	results map[string]*AnalysisResult
 }
 
 func NewServer(version string) *Server {
 	s := &Server{
 		docs:    NewDocumentStore(),
 		version: version,
+		results: make(map[string]*AnalysisResult),
 	}
 
 	s.handler = protocol.Handler{
@@ -74,21 +79,53 @@ func (s *Server) setTrace(_ *glsp.Context, params *protocol.SetTraceParams) erro
 	return nil
 }
 
-func (s *Server) textDocumentDidOpen(_ *glsp.Context, params *protocol.DidOpenTextDocumentParams) error {
-	s.docs.Open(string(params.TextDocument.URI), params.TextDocument.Text)
+func (s *Server) textDocumentDidOpen(ctx *glsp.Context, params *protocol.DidOpenTextDocumentParams) error {
+	uri := string(params.TextDocument.URI)
+	s.docs.Open(uri, params.TextDocument.Text)
+	s.analyzeAndPublish(ctx.Notify, uri)
 	return nil
 }
 
-func (s *Server) textDocumentDidChange(_ *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
+func (s *Server) textDocumentDidChange(ctx *glsp.Context, params *protocol.DidChangeTextDocumentParams) error {
+	uri := string(params.TextDocument.URI)
 	for _, change := range params.ContentChanges {
 		if c, ok := change.(protocol.TextDocumentContentChangeEventWhole); ok {
-			s.docs.Update(string(params.TextDocument.URI), c.Text)
+			s.docs.Update(uri, c.Text)
 		}
 	}
+	s.analyzeAndPublish(ctx.Notify, uri)
 	return nil
 }
 
 func (s *Server) textDocumentDidClose(_ *glsp.Context, params *protocol.DidCloseTextDocumentParams) error {
-	s.docs.Close(string(params.TextDocument.URI))
+	uri := string(params.TextDocument.URI)
+	s.docs.Close(uri)
+	s.mu.Lock()
+	delete(s.results, uri)
+	s.mu.Unlock()
 	return nil
+}
+
+func (s *Server) analyzeAndPublish(notify glsp.NotifyFunc, uri string) {
+	content, ok := s.docs.Get(uri)
+	if !ok {
+		return
+	}
+
+	result := Analyze(uri, content)
+
+	s.mu.Lock()
+	s.results[uri] = result
+	s.mu.Unlock()
+
+	go notify(protocol.ServerTextDocumentPublishDiagnostics, protocol.PublishDiagnosticsParams{
+		URI:         protocol.DocumentUri(uri),
+		Diagnostics: result.Diagnostics,
+	})
+}
+
+func (s *Server) getResult(uri string) *AnalysisResult {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return s.results[uri]
 }
